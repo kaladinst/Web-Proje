8f8a61b2a06bd08f1a69b1f02dc798a772f46589
emir <145506836+kaladinst@users.noreply.github.com>
2024-05-11 23:27:43 +0300

    Add files via upload

--- form.php
+++ form.php
@@ -5,8 +5,10 @@ $gercekemail = "g231210004@sakarya.edu.tr";
 $gercekpassword = "g231210004";
 
 if($email == $gercekemail && $password == $gercekpassword){
-    echo "Giriş Başarılı";
+    echo "Giriş Başarılı. Anasayfaya yönlendiriliyorsunuz.";
+    echo "<script>setTimeout(function(){window.location.href='index.html'}, 3000);</script>";
 }
+
 else{
     echo "E-mail veya şifre hatalı!";
 }

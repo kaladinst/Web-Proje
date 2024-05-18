f8de5cfa475acc9ff52c95ee30ab66736185dbb2
emir <emir.d10@outlook.com>
2024-05-18 21:17:02 +0300

    commit

--- iletisim.js
+++ iletisim.js
@@ -1,16 +1,38 @@
-const app = angular.module('contactApp', []);
-app.controller('contactController', function($scope) {
-    $scope.user = {};
+var app = angular.module('contactApp', []);
 
+app.controller('FormController', ['$scope', function($scope) {
+    // Initialize user object
+    $scope.user = {
+        name: '',
+        surname: '',
+        email: '',
+        gender: '',
+        country: '',
+        message: ''
+    };
+
+    $scope.submitted = false;
+
+    // Function to handle form submission
     $scope.submitForm = function() {
         if ($scope.contactForm.$valid) {
-            // Display the form data on another page or do something else with it
+            // If the form is valid, you can send the data to the server here
             console.log($scope.user);
+            $scope.submitted = true; // Only set submitted to true when the form is valid
         }
     };
 
+    // Function to reset the form
     $scope.resetForm = function() {
-        $scope.user = {};
+        $scope.user = {
+            name: '',
+            surname: '',
+            email: '',
+            gender: '',
+            country: '',
+            message: ''
+        };
         $scope.contactForm.$setPristine();
+        $scope.submitted = false;
     };
-});
+}]);
